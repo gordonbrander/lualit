39e83fdce50ae5b9b32db03d5044ac6ad8c983d3
Gordon Brander <gordonbrander@users.noreply.github.com>
2017-01-08 13:32:14 -0800

    Clean up comments

--- lualit/lit_parser.lua
+++ lualit/lit_parser.lua
@@ -1,20 +1,18 @@
-local lit = {}
+local parser = {}
 
 -- Quick and dirty way to transform a lualit string to lua string.
-function lit.parse_to_lua(lit_string)
-  -- Comment out any first line if it does not begin with a a space.
-  -- Then comment out any line that does not begin with a space.
-  -- We retain the same number of lines after replacing.
-  -- This aids with debugging, since lualit and lua file are 1:1, program
-  -- errors will show correct line numbers.
-  local lua_string = lit_string
+-- We retain the same number of lines after replacing.
+-- This aids with debugging, since lualit and lua file are 1:1, program
+-- errors will show correct line numbers.
+function parser.parse_to_lua(input_string)
+  local output_string = input_string
     -- Comment out any non-space text at beginning of lua string
     :gsub("^(%S)", "-- %1", 1)
     -- Comment any non-space text that occurs on a new line
     :gsub("(\n)(%S)", "%1-- %2")
     -- "Outdent" any indented text
     :gsub("\n    ", "\n")
-  return lua_string
+  return output_string
 end
 
-return lit
+return parser
