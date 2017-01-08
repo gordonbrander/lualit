829db21e456a20aecd8ba43105ce6ff2ad28d608
Gordon Brander <gordonbrander@users.noreply.github.com>
2017-01-08 13:57:07 -0800

    Create naive lua-to-lit parser

--- lualit/lua_parser.lua
+++ lualit/lua_parser.lua
@@ -7,32 +7,13 @@ function parser.parse_to_lit(input_string)
   -- errors will show correct line numbers.
   local output_string = input_string
     -- Indent code blocks (anything that doesn't start with "-")
-    :gsub("^([^\n]+)", "    %1", 1)
+    :gsub("^([^%-\n]+)", "    %1", 1)
     :gsub("\n([^%-\n]+)", "\n    %1")
     -- Un-comment any commented text at beginning of string
-    :gsub("\n%-%-", "\n")
+    :gsub("^%-%-%s?", "")
+    -- Un-comment any commented text at beginning of line
+    :gsub("\n%-%-%s?", "\n")
   return output_string
-end
-
-if TEST then
-  local s = [[function foo()
-  print('x')
-end
-
--- Foo bar baz
-
--- baz bing bazang
-
-for x in ipairs(y)
-  x = x - x
-end
-
--- bazoong bazong
--- bla
---
--- Bloo
-]]
-  print(parser.parse_to_lit(s))
 end
 
 return parser
