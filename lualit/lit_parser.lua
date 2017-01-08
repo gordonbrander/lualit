7184344c80dc7bd9843f57c54588fef69bdc1ff7
Gordon Brander <gordonbrander@users.noreply.github.com>
2017-01-08 13:13:42 -0800

    Remove fencing to be docco compatible

--- lualit/lit_parser.lua
+++ lualit/lit_parser.lua
@@ -1,10 +1,4 @@
 local lit = {}
-local stringtools = require("lualit.stringtools")
-
--- Replace fences with newlines and pad everything inside
-local function replace_fenced_blocks(match)
-  return "\n\n" .. stringtools.pad(match, "    ") .. "\n"
-end
 
 -- Quick and dirty way to transform a lualit string to lua string.
 function lit.parse_to_lua(lit_string)
@@ -14,8 +8,6 @@ function lit.parse_to_lua(lit_string)
   -- This aids with debugging, since lualit and lua file are 1:1, program
   -- errors will show correct line numbers.
   local lua_string = lit_string
-    -- Replace fenced code
-    :gsub("\n```lua\n(.-)\n```\n", replace_fenced_blocks)
     -- Comment out any non-space text at beginning of lua string
     :gsub("^(%S)", "-- %1", 1)
     -- Comment any non-space text that occurs on a new line
